0e711ee49bcd70b454f2658243adb370cc62eba2
SAMID AMAURY BARRERA CAMARGO <sabarrera21@ucatolica.edu.co>
2024-02-11 09:59:37 -0500

    Clases Avestruz, Gallina y Canario con el metodo vuela por herencia

--- src/Avestruz.java
+++ src/Avestruz.java
@@ -1,5 +1,10 @@
 public class Avestruz extends Ave{
     @java.lang.Override
+    public void vuela() {
+        System.out.println("no vuela");
+    }
+
+    @java.lang.Override
     public void makeSound() {
         System.out.println("<<Silencio>>");
     }
--- src/Gallina.java
+++ src/Gallina.java
@@ -1,5 +1,10 @@
 public class Gallina extends Ave{
     @java.lang.Override
+    public void vuela() {
+        System.out.println("Vuela con alas");
+    }
+
+    @java.lang.Override
     public void makeSound() {
         System.out.println("Grasna");
     }
